6790ae5a9dc365f861af4224d8cba7cd28b7ad9d
Park Yoon <107896206+notorious-park@users.noreply.github.com>
2022-06-23 17:35:57 +0900

    Delete day08 directory

--- day08/07.파일 삭제하기.py
+++
@@ -1,9 +0,0 @@
-# 파일 삭제하기
-from os import remove
-
-target_file = './images/전북은행_로고_copy.jpg'
-k = input('{} 파일을 삭제하시겠습니까? ([y],n)'.format(target_file))
-
-if k == 'y' or k == '':
-    remove(target_file)
-    print('{} 파일을 삭제하였습니다'.format(target_file))
--- day08/a1.py
+++
@@ -1,1 +0,0 @@
-fields = row.split(",")
